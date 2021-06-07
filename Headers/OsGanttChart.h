d8f8de4e2ce4f7f45cf9171b989c4389ca36c9f8
minsoo kim <zlemzlem5656@naver.com>
2021-06-07 17:37:49 +0900

    [삭제] 터미널 간트 차트 사용하지 않음으로 제어처리 삭제

--- Headers/OsGanttChart.h
+++ Headers/OsGanttChart.h
@@ -13,41 +13,29 @@
 void draw_gantt_chart(Process *arr, int process_count, int max_time)
 {
     int chart_option;
-    printf("간트차트 옵션을 입력하세요: 0-> 비율 / 1-> 대기시간");
-    scanf("%d", &chart_option);
 
-    // CPU 위에 올라온 상태만 그리는 방식
-    if (chart_option == 0)
+    for (int i = 0; i < process_count; i++)
     {
-        printf("tlqkf");
-    }
-    // 대기 시간을 고려한 방법
-    else
-    {
+        if (arr[i].process_id < 10)
+            printf("PID 0%d |", arr[i].process_id);
+        else
+            printf("PID %d |", arr[i].process_id);
 
-        for (int i = 0; i < process_count; i++)
+        for (int j = 0; j < arr[i].end_time + 1; j++)
         {
-            if (arr[i].process_id < 10)
-                printf("PID 0%d |", arr[i].process_id);
-            else
-                printf("PID %d |", arr[i].process_id);
-
-            for (int j = 0; j < arr[i].end_time + 1; j++)
-            {
-                printf(" %c ", arr[i].gantt[j]);
-            }
-            printf("\n");
-        }
-        printf("        "); // PID %d 만큼 띄어쓰기
-        for (int i = 0; i < max_time; i++)
-        {
-            if (i < 10)
-                printf("0%d ", i);
-            else
-                printf("%d ", i);
+            printf(" %c ", arr[i].gantt[j]);
         }
         printf("\n");
     }
+    printf("        "); // PID %d 만큼 띄어쓰기
+    for (int i = 0; i < max_time; i++)
+    {
+        if (i < 10)
+            printf("0%d ", i);
+        else
+            printf("%d ", i);
+    }
+    printf("\n");
 }
 
 #endif
