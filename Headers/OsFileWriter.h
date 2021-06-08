92eba8184c10f1c53893f3b2baeaa40327a9c78c
minsoo kim <zlemzlem5656@naver.com>
2021-06-08 16:30:23 +0900

    [수정, 추가] 출력 파일 형식 변경

--- Headers/OsFileWriter.h
+++ Headers/OsFileWriter.h
@@ -7,10 +7,11 @@
 
 typedef struct Optimal
 {
-    int average_time;
-    double return_time;
-    double optimal_data;
-    int optimal_quantum;
+    int run_time;        // 총 실행시간
+    double average_time; // 평균 대기시간
+    double return_time;  // 평균 반환시간
+    double optimal_data; // 비교 지표
+    int optimal_quantum; //최적의 quantum
 } Optimal;
 
 Optimal optimal_result;
@@ -25,50 +26,47 @@ FILE *create_output_file()
     fprintf(output_file, "# RR Scheduler Output File\n");
     fprintf(output_file, "%d년 %d월 %d일 %d시 %d분 실행\n\n", t->tm_year + 1900, t->tm_mon + 1, t->tm_mday, t->tm_hour, t->tm_min);
 
-    fprintf(output_file, "## Restult \n");
-    fprintf(output_file, "| time quantum |context switching|평균 대기시간 | 평균 반환시간| 실행 시간|\n");
-    fprintf(output_file, "| ---|---| ---| ---| ---|\n");
+    // fprintf(output_file, "## Restult \n");
+    // fprintf(output_file, "| time quantum |context switching|평균 대기시간 | 평균 반환시간| 실행 시간|\n");
+    // fprintf(output_file, "| ---|---| ---| ---| ---|\n");
 
     optimal_result.optimal_data = 9999;
 
     return output_file;
 }
 
-void concat_output_file(FILE *output_file, int quantum, int count, Process *process_list, double average_time, double return_time, int time, int context_switching)
+void concat_output_file(FILE *output_file, int quantum, int count, double average_time, double return_time, int time, int context_switching)
 {
 
-    if (optimal_result.return_time == return_time && optimal_result.average_time == average_time)
+    if (optimal_result.run_time == time && optimal_result.return_time == return_time && optimal_result.average_time == average_time)
     {
         return;
     }
 
     // 프로세스별로 출력
 
-    // fprintf(output_file, "## Time Quantum: %d\n\n", quantum);
-    // fprintf(output_file, "context switching 횟수: %d\n\n", context_switching);
+    fprintf(output_file, "## Time Quantum: %d\n\n", quantum);
+    fprintf(output_file, "context switching 횟수: %d\n\n", context_switching);
 
-    // fprintf(output_file, "|프로세스 id| 도착시간|서비스 시간| 종료 시간| 반환 시간| 정규화된 반환 시간|\n");
-    // fprintf(output_file, "|---| ---|---| ---| ---| ---|\n");
+    fprintf(output_file, "|프로세스 id| 도착시간|서비스 시간| 종료 시간| 반환 시간| 정규화된 반환 시간|\n");
+    fprintf(output_file, "|---| ---|---| ---| ---| ---|\n");
 
-    // for (int i = 0; i < count; i++)
-    // {
-    //     fprintf(output_file, "|%d| %ds|  %ds|  %ds| %ds| %.2fs |\n", process_list[i].process_id, process_list[i].arrival_time, process_list[i].burst_duration,
-    //             process_list[i].end_time, process_list[i].end_time - process_list[i].arrival_time, (process_list[i].end_time - process_list[i].arrival_time) / (double)process_list[i].arrival_time);
-    // }
+    for (int i = 0; i < count; i++)
+    {
+        fprintf(output_file, "|%d| %ds|  %ds|  %ds| %ds| %.2fs |\n", g_process[i].process_id, g_process[i].arrival_time, g_process[i].burst_duration,
+                time, g_process[i].end_time - g_process[i].arrival_time, (g_process[i].end_time - g_process[i].arrival_time) / (double)g_process[i].arrival_time);
+    }
 
-    //
-    fprintf(output_file, "|%d| %d회| %.1fs| %.1fs| %ds |\n", quantum, context_switching, average_time, return_time, time);
+    fprintf(output_file, "\n---\n");
 
-    // fprintf(output_file, "\n---\n");
-
-    // fprintf(output_file, "**총 실행 시간**: %d\n", time);
-    // fprintf(output_file, "**평균 대기시간**: %d\n", average_time);
-    // fprintf(output_file, "**평균 반환시간**: %.2f\n", return_time);
+    fprintf(output_file, "**총 실행 시간**: %d\n", time);
+    fprintf(output_file, "**평균 대기시간**: %.2f\n", average_time);
+    fprintf(output_file, "**평균 반환시간**: %.2f\n", return_time);
 
     // 최종 결과만 출력
 
     // 반환 시간에 가중치를 주는 방식
-    int tmp_data = average_time + time;
+    int tmp_data = average_time + return_time * 9;
 
     if (tmp_data < optimal_result.optimal_data)
     {
@@ -76,6 +74,7 @@ void concat_output_file(FILE *output_file, int quantum, int count, Process *proc
         optimal_result.return_time = return_time;
         optimal_result.optimal_data = tmp_data;
         optimal_result.optimal_quantum = quantum;
+        optimal_result.run_time = time;
     }
 }
 
@@ -83,7 +82,8 @@ void finish_output_file(FILE *output_file)
 {
     fprintf(output_file, "## Optimal Time quantum\n");
     fprintf(output_file, "**최적의 time quantum**: %d\n", optimal_result.optimal_quantum);
-    fprintf(output_file, "**평균 대기시간**: %d\n", optimal_result.average_time);
+    fprintf(output_file, "**총 실행 시간**: %d\n", optimal_result.run_time);
+    fprintf(output_file, "**평균 대기시간**: %.2f\n", optimal_result.average_time);
     fprintf(output_file, "**평균 반환시간**: %.2f\n", optimal_result.return_time);
 }
 
--- README.md
+++ README.md
@@ -11,41 +11,55 @@
 ```c
 typedef struct process
 {
-    int process_id;    // 프로세스 번호
-    int arrivalTime;   // 도착 시간
-    int burstDuration; // 수행 시간
-    int remain_time;   // 수행 후 남아있는 시간
-    int waiting_time;  // 대기 시간
-    int end_time;      // 종료 시간
-    int judge;         // 실행 판단 (1: 실행, 0: 종료)
-    int deadlineTime;  // 데드라인 (실시간)
+    int process_id;     // 프로세스 번호
+    int arrival_time;   // 도착 시간
+    int burst_duration; // 수행 시간
+    int remain_time;    // 수행 후 남아있는 시간
+    int waiting_time;   // 대기 시간
+    int end_time;       // 종료 시간
+    int judge;          // 실행 판단 (1: 실행, 0: 종료)
+    int deadline_time;  // 데드라인 (실시간)
+    // char gantt[MAX_PROCESS]; -> 터미널 gantt chart 사용 안 하는 것으로 결정
 } process;
 ```
 
 ## use flow
 
 1. input.txt 파일을 읽어온다.
-2. `,`으로 데이터를 구분해서 프로세스 정보를 저장한다.
-3. 프로세스 큐에(ready queue) 넣어준다.
+2. `,`으로 데이터를 구분해서 프로세스 정보를 저장한다. (데드라인도 저장은 하지만 사용은 하지 않음)
+3. 프로세스 큐에(ready queue) 넣어준다. (g_process)
 4. time quantum 은 `scanf` 를 통해 사용자에게 입력을 직접 받는다.
 5. RR 스케줄링을 통해 프로세스를 실행 시켜준다.
-6. Gantt Chart 를 통해 결과를 확인한다. (구현 예정)
+   1. 사용자에게 입력받은 time quantum 까지 1 에서 키워가며 실행시킵니다.
+   2. 평균 대기시간 + 평균 반환시간 + 총 실행시간 에 가중치를 두어 최적의 time quantum 을 찾습니다.
 
 ## How to Use
 
 ```bash
-gcc <YOUR-PATH>/scheduler.c
-./a.out
+gcc -o scheduler.out <YOUR-PATH>/scheduler.c
+./scheduler.out [quantum-max]
 ```
 
-## output.txt
+## output.md
 
-| pid       | Arrival_time | CPU_time  | return time | reg return time |
-| --------- | ------------ | --------- | ----------- | --------------- |
-| \<entry\> | \<entry\>    | \<entry\> | \<entry\>   | \<entry\>       |
+### Time quantum $
 
-평균 대기시간: `int`
-평균 반환시간: `double`
+context switching 횟수: $
+
+| pid       | Arrival_time | CPU_time  | end_time  | return time | reg return time |
+| --------- | ------------ | --------- | --------- | ----------- | --------------- |
+| \<entry\> | \<entry\>    | \<entry\> | \<entry\> | \<entry\>   | \<entry\>       |
+
+**총 실행 시간**: `int`
+**평균 대기시간**: `double`
+**평균 반환시간**: `double`
+
+### Optimal Time quantum
+
+**최적의 time quantum**: `int`
+**총 실행 시간**:`int`
+**평균 대기시간**:`double`
+**평균 반환시간**:`double`
 
 ## 참고 문헌 및 자료
 
